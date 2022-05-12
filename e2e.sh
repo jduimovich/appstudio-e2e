3f8c689c13c3b37b1c99853387747f603ef76472
John Duimovich <jduimovich@gmail.com>
2022-05-12 08:04:32 -0400

    cleanup

--- e2e.sh
+++ e2e.sh
@@ -1,6 +1,8 @@
 
 oc apply -f namespace.yaml 
-oc project app-e2e
+# Figured out the namespace from the yaml
+NS=$(yq e '.metadata.name' namespace.yaml)
+oc project $NS
 oc apply -f app/application.yaml 
 bash install-secret.sh
 sleep 10

4fac2a3825361c68659c96370c23618683657b09
John Duimovich <jduimovich@gmail.com>
2022-05-27 13:42:02 -0400

    wip

--- e2e.sh
+++ e2e.sh
@@ -1,4 +1,14 @@
 
+# This script will determine the NS and the current repo
+# so that this work in forks.
+
+APP=$1
+if [ -z $APP ]
+then
+  APP=app 
+exit
+echo "running e2e app defined in the $APP directory"
+
 oc apply -f namespace.yaml 
 # Figured out the namespace from the yaml
 NS=$(yq e '.metadata.name' namespace.yaml)
@@ -7,4 +17,8 @@ oc apply -f app/application.yaml
 bash install-secret.sh
 sleep 10
 oc apply -f app 
-oc apply -f gitops 
+
+REPO=$(git config --get remote.origin.url)
+yq e '.spec.destination.namespace="'$NS'"' gitops/extras.yaml |
+    yq e '.spec.source.repoURL="'$REPO'"' - | 
+    oc apply -f - 
